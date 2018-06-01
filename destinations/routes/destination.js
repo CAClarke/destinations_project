f63a38bdb833809977ae5ffd8f2fcd6e54014e18
CAClarke <azilver@hotmail.com>
2018-06-01 19:14:40 +0200

    loop links in index

--- destinations/routes/destination.js
+++ destinations/routes/destination.js
@@ -8,26 +8,36 @@ var Destination = require('../models/new_destination,');
 
    //get single post by id
   router.get('/',function(req,res){
-    // let destination_id = req.param('id');
     
-    Destination.destinations.Find(destination, function(error, post){
+     // get id parameter
+      // let this_url =window.location.href;
+      // console.log(this_url);
+      // var url_string = this_url;
+      // var url = new URL(url_string);
+      var destination_id = req.params.id;
+
+      console.log(destination_id);
+
+    Destination.findById({destination_id}, function(error, destination){
       console.log(destination);
         if(error){
             res.send(error);
         }
         else{
-            res.render('pages/destination',{
-            //    destination_backdrop: destination.destination_backdrop,
-            //    destination_city: destination.destination_city,
-            //    destination_text: destination.destination_text,
-            //    destination_long: destination.destination_long,
-            //    destination_lat: destination.destination_lat,
-            //    destination_img1 = destination.destination_img1,
-            //    destination_img2 = destination.destination_img2,
-            //    destination_img3 = destination.destination_img3,
-            //    backdrop_img = destination.backdrop_img
-            // });
-        // }
+            res.render('pages/destination',{title: 'city', destination:destination}
+          )};
+
+    //         //    destination_backdrop: destination.destination_backdrop,
+    //         //    destination_city: destination.destination_city,
+    //         //    destination_text: destination.destination_text,
+    //         //    destination_long: destination.destination_long,
+    //         //    destination_lat: destination.destination_lat,
+    //         //    destination_img1 = destination.destination_img1,
+    //         //    destination_img2 = destination.destination_img2,
+    //         //    destination_img3 = destination.destination_img3,
+    //         //    backdrop_img = destination.backdrop_img
+            });
+            
       });
   //    var destination = new Destination();
   //  destination.backdrop = req.body.destination_backdrop;
@@ -53,8 +63,9 @@ var Destination = require('../models/new_destination,');
     //   res.send(user_id);
     // });
     
-  res.render('pages/destination',{title: 'Explore', items: 'resultsArray'});
+//   res.render('pages/destination',{title: 'Explore', items: 'resultsArray'});
 
+// // });
 // });
 
 // var assert = require('assert');
@@ -70,36 +81,12 @@ var Destination = require('../models/new_destination,');
 //     }, function(){
 //       db.close();
 //       res.render(index, {items:resultArray});
+//       res.render(index, {items:resultArray});
 //     });
 //   });
 // });
 
 
-// var url = 'mongoose:';
-// router.post('/insert', function(req,res,next){
-//   var item = {
-//     title:req.body.title,
-//     content:req.body.content,
-//     author:req.body.author
-//   };
-//   mongo.connect(url,function(err,db){
-//     assert.equal(null,err);
-//     db.collection('collection-name').insertOne(item,function(){
-//       assert.equal(null,error);
-//       consol.log('item inserted');
-//       db.close();
-//     });
-//   });
-// })
- 
-//  //get id
-//  let this_url =window.location.href;
-//       console.log(this_url)
-//       var url_string = this_url;
-//       console.log(url_string)
-//       var url = new URL(url_string);
-//       var id = url.searchParams.get("id");
-//       console.log(id);
 
 
 
--- destinations/routes/index.js
+++ destinations/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+var Destination = require('../models/new_destination,');
 
 //homepage
 router.get('/', function(req, res, next){
-  res.render('pages/index',{title: 'Tri Tours'});
+
+  Destination.find({}, function(error, destinations){
+    console.log(destinations);
+      if(error){
+          res.send(error);
+      }
+      else{
+          res.render('pages/index',{title: 'city', destinations:destinations}
+        )};
+
+
+});
+
+  // res.render('pages/index',{title: 'Tri Tours'});
 });
 
 
