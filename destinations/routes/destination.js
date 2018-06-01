2fbba7eeeac602b2cd3df69fe43c870613674823
CAClarke <azilver@hotmail.com>
2018-06-01 22:55:49 +0200

    for map made vars, refuses to apply stylesheets in dest.ejs pages

--- destinations/routes/destination.js
+++ destinations/routes/destination.js
@@ -22,51 +22,8 @@ var Destination = require('../models/new_destination,');
         else{
             res.render('pages/destination',{title: 'Explore', destinations:destinations}
           )};
-
-    //         //    destination_backdrop: destination.destination_backdrop,
-    //         //    destination_city: destination.destination_city,
-    //         //    destination_text: destination.destination_text,
-    //         //    destination_long: destination.destination_long,
-    //         //    destination_lat: destination.destination_lat,
-    //         //    destination_img1 = destination.destination_img1,
-    //         //    destination_img2 = destination.destination_img2,
-    //         //    destination_img3 = destination.destination_img3,
-    //         //    backdrop_img = destination.backdrop_img
-            });
+        });
             
       });
-  //    var destination = new Destination();
-  //  destination.backdrop = req.body.destination_backdrop;
-  //  destination.city = req.body.destination_city;
-  //  destination.text = req.body.destination_text;
-  //  destination.long = req.body.destination_long;
-  //  destination.lat = req.body.destination_lat;
-  //  destination.gallery = req.body.destination_img;
-    // });
-
-
-
-    // // get parameters
-    // router.get('/destinations', function(req, res, next) {
-    //   var destination_id = req.param('id');
-    //   var destination_backdrop = req.param('backdrop');
-    //   var destination_city = req.param('city');  
-    //   var destination_text = req.param('text');  
-    //   var destination_long = req.param('long');  
-    //   var destination_lat = req.param('lat');  
-    //   var destination_img = req.param('gallery');  
-
-    //   res.send(user_id);
-    // });
-    
-//   res.render('pages/destination',{title: 'Explore', items: 'resultsArray'});
-
-// // });
-// });
-
-
-
-
-
 
 module.exports = router;
